063cbd9eef41945e3011e25d9f62a6bc7c82b6cd
ziaxgit <112583896+ziaxgit@users.noreply.github.com>
2024-09-18 23:17:24 +0100

    restyle home page

--- Frontend/src/Components/Home.tsx
+++ Frontend/src/Components/Home.tsx
@@ -3,7 +3,7 @@ import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import artInstituteOfChicagoImage from "../assets/wide_med_art-institute-of-chicago-01-2.jpg";
 import clevelandMuseumImage from "../assets/ClevelandMuseum_exterior.jpg";
 import { useNavigate } from "react-router-dom";
-
+import "../index.css";
 export default function Home() {
   let navigate = useNavigate();
 
@@ -29,7 +29,67 @@ export default function Home() {
           offers a rich and immersive experience tailored to your interests.
         </p>
       </Container>
-      <Container>
+      <Container className="mt-5">
+        <Row>
+          <Col>
+            <Card className="d-flex flex-row flex-wrap border-0 shadow gap-">
+              <Col xs={12} md={6}>
+                <img
+                  src={clevelandMuseumImage}
+                  alt="Image"
+                  className="img-fluid museum-img"
+                />
+              </Col>
+
+              <Col xs={12} md={6} className="p-3">
+                <h3 className="card-title mb-4">Cleveland Museum of Art</h3>
+                <p className="mb-4">
+                  The Cleveland Museum of Art, established in 1913, is a
+                  distinguished art museum located in Cleveland, Ohio. Renowned
+                  for its diverse and comprehensive collection, the museum
+                  features over 61,000 artworks from various cultures and time
+                  periods. Highlights include pieces by iconic artists such as
+                  Caravaggio, Monet, and Picasso.
+                </p>
+                <Button onClick={() => handleClick("clevelandmuseum")}>
+                  Browse
+                </Button>
+              </Col>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+      <Container className="mt-5">
+        <Row>
+          <Col>
+            <Card className="d-flex flex-row flex-wrap border-0 shadow gap-">
+              <Col xs={12} md={6} className="p-3">
+                <h3 className="card-title mb-4">Art Institute of Chicago</h3>
+                <p className="mb-4">
+                  The Chicago Art Institute, founded in 1879, is a premier art
+                  museum located in the heart of Chicago. It boasts an extensive
+                  collection of artwork from around the world, featuring
+                  masterpieces by artists such as Vincent van Gogh, Grant Wood,
+                  and Georges Seurat. Known for its rich educational programs
+                  and public engagement initiatives.
+                </p>
+                <Button onClick={() => handleClick("chichagoart")}>
+                  Browse
+                </Button>
+              </Col>
+              <Col xs={12} md={6}>
+                <img
+                  src={artInstituteOfChicagoImage}
+                  alt="Image"
+                  className="img-fluid museum-img"
+                />
+              </Col>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+
+      {/* <Container>
         <h2 className="text-center">Let's explore...</h2>
         <Row className="mt-4 justify-content-center">
           <Col xs={10} sm={12} md={4} className="mb-4">
@@ -89,7 +149,7 @@ export default function Home() {
             </Card>
           </Col>
         </Row>
-      </Container>
+      </Container> */}
     </div>
   );
 }
