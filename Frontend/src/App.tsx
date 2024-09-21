2fc4d9dda700f33757da05357d47c3f8cdbe3a36
ziaxgit <112583896+ziaxgit@users.noreply.github.com>
2024-09-21 14:18:24 +0100

    clean up unused code

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,24 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 import NavBar from "./Components/NavBar";
 import Home from "./Components/Home";
 import Collections from "./Components/Collections";
 import Exhibitions from "./Components/Exhibitions";
-import { getExhibitionsFromLocalStorage } from "./utils/exhibitionStorage";
-import ExhibitionCard from "./Components/ExhibitionDetail";
 import ArtList from "./Components/ArtList";
 import ChicagoArtList from "./Components/ChicagoArtList";
 import ExhibitionDetail from "./Components/ExhibitionDetail";
 
-interface Exhibition {
-  name: string;
-  path: string;
-}
-
 const App: React.FC = () => {
-  const [exhibitions] = useState<Exhibition[]>(
-    getExhibitionsFromLocalStorage()
-  );
   return (
     <>
       <NavBar />
