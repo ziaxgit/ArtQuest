22f814fe8aee41b217c401eb5578232b001c6b2d
ziaxgit <112583896+ziaxgit@users.noreply.github.com>
2024-09-21 21:57:55 +0100

    added hero section img

--- Frontend/src/Components/HeroSection.tsx
+++ Frontend/src/Components/HeroSection.tsx
@@ -1,25 +1,30 @@
 import { Container, Button } from "react-bootstrap";
 import "../index.css"; // Custom styles for the hero section
+import heroImg from "../assets/hero-img.png";
 
 function HeroSection() {
   return (
-    <div className="hero-section text-center text-white">
-      <div className="overlay"></div>
-      <Container className="hero-content d-flex flex-column align-items-center justify-content-center">
-        <h1 className="display-5 fw-bold">Unleash Your Inner Curator</h1>
-        <p className="lead mt-3">
-          Welcome to ArtQuest, a groundbreaking platform that empowers you to
-          create personalized virtual exhibitions from the extensive collections
-          of the world's leading museums and universities. Whether you're a
-          researcher, student, or an art enthusiast, ArtQuest brings the art
-          world to your fingertips.
-        </p>
-        <div className="mt-4">
-          <Button href="#explore" variant="primary" size="lg" className="me-2">
-            Start Your ArtQuest
-          </Button>
+    <div className="hero-section-container text-center mt-4">
+      <Container>
+        <div className="hero-text-div text-black-50">
+          <h1 className="">Unleash Your Inner Curator</h1>
+          <p className="lead text-black-50">
+            Welcome to ArtQuest, a groundbreaking platform that empowers you to
+            create personalized virtual exhibitions from the extensive
+            collections of the world's leading museums and universities. Whether
+            you're a researcher, student, or an art enthusiast, ArtQuest brings
+            the art world to your fingertips.
+          </p>
+          <div className="mt-4 hero-btn">
+            <Button href="#explore" variant="primary" className="me-2">
+              Start Your ArtQuest
+            </Button>
+          </div>
         </div>
       </Container>
+      <div className="hero-img-div">
+        <img src={heroImg}></img>
+      </div>
     </div>
   );
 }
